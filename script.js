e56d6dd33456df87d6734580f9a354d5259bc8c0
Anuja M <124621141+Anuja-M123@users.noreply.github.com>
2023-07-25 12:21:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+       red = ()=> document.getElementById("img").setAttribute('src',"https://images.barrons.com/im-552328?width=700&height=476")
+       black =()=> document.getElementById('img').setAttribute('src',"https://www.motorbeam.com/wp-content/uploads/2023-Rolls-Royce-Phantom-Side.jpg");
+       white = ()=> document.getElementById("img").setAttribute('src',"https://imgd.aeplcdn.com/1280x720/cw/ec/33413/RollsRoyce-Phantom-Exterior-121314.jpg?wm=0")
+    </script>
